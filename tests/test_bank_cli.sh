89427e7b80c9ace31c589345448125f6a4382acf
yowgf <alexthomasmol@gmail.com>
2022-07-01 20:20:38 -0300

    Fix tests

--- tests/test_bank_cli.sh
+++ tests/test_bank_cli.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 tempresults=test_bank_cli_temp
 tempbank=tempbank
@@ -8,8 +8,17 @@ tempstore=tempstore
 inputfpath=fixtures/inputs_bank_cli_1.txt
 outputfpath=fixtures/outputs_bank_cli_1.txt
 
+function cleanup_procs {
+    echo Cleaning up processes
+    processes=$(lsof -i :5555,5556 | tail -n +2 | awk '{print $2}')
+    [ -z "$processes" ] && return
+    kill -9 $processes
+}
+
 nohup ./debug_bank_srv.sh 2>&1 > $tempbank &
 nohup ./debug_store_srv.sh 2>&1 > $tempstore &
+
+sleep 0.2
 
 cat $inputfpath | ./debug_bank_cli.sh > $tempresults
 
@@ -18,12 +27,14 @@ expected=$(cat $outputfpath)
 
 if [ "$actual" != "$expected" ]; then
     echo Test failed. Displaying diff below.
-    diff $tempresults $outputfpath
+    diff $tempresults $outputfpath || true
+    cleanup_procs
     exit 1
 else
     echo Test succeeded
 fi
 
+cleanup_procs
 rm $tempbank
 rm $tempstore
 rm $tempresults
